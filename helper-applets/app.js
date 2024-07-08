3785a708821cd9494270911b96f494560437ecdf
JieFangen <jiefangen@gmail.com>
2024-07-08 12:13:24 +0800

    小程序框架调整补充

--- helper-applets/app.js
+++ helper-applets/app.js
@@ -1,5 +1,3 @@
-const themeListeners = []
-
 App({
   onLaunch: function () {
     console.log('App Launch')
@@ -15,23 +13,6 @@ App({
   },
   onHide: function () {
       console.log('App Hide')
-  },
-  themeChanged(theme) {
-      this.globalData.theme = theme
-      themeListeners.forEach((listener) => {
-          listener(theme)
-      })
-  },
-  watchThemeChange(listener) {
-      if (themeListeners.indexOf(listener) < 0) {
-          themeListeners.push(listener)
-      }
-  },
-  unWatchThemeChange(listener) {
-    const index = themeListeners.indexOf(listener)
-    if (index > -1) {
-        themeListeners.splice(index, 1)
-    }
   },
   globalData: {
     hasLogin: false,
